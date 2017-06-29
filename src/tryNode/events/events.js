2abddb52db07a0290a5c392ff15bb9ba304202f4
关晓丹 <GXD9983@163.com>
2017-06-29 21:40:14 +0800

    node 事件循环初步

---
+++ src/tryNode/events/events.js
@@ -0,0 +1,33 @@
+/**
+ * Created by GXD on 2017/6/29.
+ * node.js事件循环
+ * 1.事件处理程序
+ * 2.触发某个事件
+ */
+
+// 引入events模块
+var events = require('events')
+
+// 创建eventEmitter对象
+var eventEmitter = new events.EventEmitter()
+
+// 创建事件处理程序
+var eventHandler = function () {
+  console.log('连接成功。。。。')
+  // 触发data_received事件
+  eventEmitter.emit('data_received')
+  console.log('连接成功222222.。。。。。')
+}
+
+// 绑定 connection 事件处理程序
+eventEmitter.on('connection', eventHandler)
+
+// 使用匿名函数绑定 data_received 事件
+eventEmitter.on('data_received', function () {
+  console.log('数据接收成功')
+})
+
+// 触发 connection 事件
+eventEmitter.emit('connection')
+
+console.log('程序执行完毕！！！')
