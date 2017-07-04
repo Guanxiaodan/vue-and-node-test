dd7374b5e05431c2e7bcfc1876aa6b0c85dfd94c
关晓丹 <GXD9983@163.com>
2017-07-04 19:44:38 +0800

    Vue checkbox 测试

---
+++ src/pages/vueCheckbox.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div>
+    <p>{{choosed}}</p>
+    <ul>
+      <li v-for="m in msg">
+        <input type="checkbox"  :value="m" v-model="choosed">{{m}}
+      </li>
+    </ul>
+  </div>
+
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: ['荔枝', '西瓜', '樱桃', '蓝莓', '木瓜', '芒果', '香橙', '桂圆', '红龙果', '香蕉'],
+        choosed: []
+      }
+    }
+  }
+</script>
+
+<style>
+  li {
+    list-style: none;
+  }
+</style>
