64f172ae336b5269dcf410434054ccc0038ebd6f
关晓丹 <GXD9983@163.com>
2017-10-09 10:49:05 +0800

    buffer就先到这里，不全，基本的是这，详细的需要查看文档

--- src/tryNode/buffer/buffer.js
+++ src/tryNode/buffer/buffer.js
@@ -4,13 +4,16 @@
  * 运行：进入 vue-and-node-test/src/tryNode/buffer/，，， 执行 node buffer.js
  */
 
-// 创建buffer实例并写入buffer缓存
+/**
+ * 创建buffer实例并写入buffer缓存
+ */
 var buf = new Buffer(226);
 var len = buf.write('www.runmmb.com');
-console.log('写入的字节数：'+ len);
+console.log('写入的字节数：' + len);
 
-
-// 从缓存区返回字符串
+/**
+ * 从缓存区返回字符串
+ */
 var buffer = new Buffer(26)
 for (var i = 0; i < 26; i++) {
   buffer[i] = i + 97
@@ -20,9 +23,18 @@ console.log(buffer.toString('ascii')); // abcdefghijklmnopqrstuvwxyz
 console.log(buffer.toString('ascii', 0, 5)) // abcde
 console.log(buffer.toString('utf8', 0, 5)) // abcde
 
-
-// 将buffer转换为json对象
+/**
+ * 将buffer转换为json对象
+ */
 var buff = new Buffer('guanguantongxue');
 var json = buff.toJSON();
 console.log(json) // { type: 'Buffer',data: [ 103, 117, 97, 110, 103, 117, 97, 110, 116, 111, 110, 103, 120, 117, 101 ] }
 
+/**
+ * 缓冲区合并
+ * */
+var bu1 = new Buffer('my name is ')
+var bu2 = new Buffer('dandan')
+var bu3 = Buffer.concat([bu1, bu2])
+console.log('这是合并后的结果', bu3) // 这是合并后的结果 <Buffer 6d 79 20 6e 61 6d 65 20 69 73 64 61 6e 64 61 6e>
+console.log('这是合并后的结果', bu3.toString()) // 这是合并后的结果 my name is dandan
