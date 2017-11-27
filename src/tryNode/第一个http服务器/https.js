7a30a57fb256dd44abe1dcb1ad94bc91022b20c7
关晓丹 <GXD9983@163.com>
2017-11-27 11:13:06 +0800

    https没有证书

---
+++ src/tryNode/第一个http服务器/https.js
@@ -0,0 +1,16 @@
+/**
+ * Created by GXD on 2017/11/27.
+ *
+ */
+var https = require('https')
+var fs = require('fs')
+
+var options = {
+  key: fs.readFileSync('ssh_key.pem'), // 私钥文件
+  cert: fs.readFileSync('ssh_cert.pem') // 这个是证书文件
+}
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200)
+  res.end('HTTPS服务器')
+}).listen(4444)
