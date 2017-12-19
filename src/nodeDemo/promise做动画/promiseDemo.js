23a4fd96a9053ad4b93fd10cc50acf69fe35e3f1
关晓丹 <GXD9983@163.com>
2017-12-19 11:35:48 +0800

    promiseDemo

---
+++ src/nodeDemo/promise做动画/promiseDemo.js
@@ -0,0 +1,65 @@
+/**
+ * Created by GXD on 2017/12/19.
+ *
+ */
+
+/**
+ * 例子一
+ */
+// new Promise((resolve, reject) => {
+//   console.log('我是老大');
+//   resolve('这是来自老大的数据')
+// }).then((data) => {
+//   console.log('我是老二')
+//   console.log(data)
+// }).catch((err) => {
+//   console.log('发生了错误', err)
+// })
+
+/**
+ * 例子二
+ * 链式.then()
+ * 返回promise对象
+ */
+// new Promise((resolve, reject) => {
+//   console.log('我是老大')
+//   resolve('这是来自老大的数据')
+// })
+//   .then((data) => {
+//     console.log('我是老二')
+//     console.log(data)
+//     return new Promise((resolve) => {
+//       resolve('这是来自老二的数据')
+//     })
+//   })
+//   .then((data) => {
+//     console.log('我是老三')
+//     console.log(data)
+//   })
+//   .catch((err) => {
+//     console.log('发生了错误', err)
+//   })
+
+
+
+/**
+ * 例子三
+ * 链式.then()
+ * 返回非promise对象
+ */
+new Promise((resolve, reject) => {
+  console.log('我是老大')
+  resolve('这是来自老大的数据')
+})
+  .then((data) => {
+    console.log('我是老二')
+    console.log(data)
+    return '这是来自老二的数据'
+  })
+  .then((data) => {
+    console.log('我是老三')
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log('发生了错误', err)
+  })
