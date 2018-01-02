3b255b25755a7265595e3e26819f6a3514d9deb0
关晓丹 <GXD9983@163.com>
2018-01-02 19:05:50 +0800

    该写web模块下面的例子了

---
+++ src/tryNode/web模块/web服务器.js
@@ -0,0 +1,12 @@
+/**
+ * Created by GXD on 2018/1/2.
+ *
+ */
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+// 创建服务器
+http.createServer(function (request, response) {
+  //
+})
