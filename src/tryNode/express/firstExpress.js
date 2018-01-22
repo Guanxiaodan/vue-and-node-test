71f371561d548ac7ae81418e05a3b4744bec4ff9
关晓丹 <GXD9983@163.com>
2018-01-22 20:10:14 +0800

    firstExpress

--- src/tryNode/express/firstExpress.js
+++ src/tryNode/express/firstExpress.js
@@ -1,6 +1,13 @@
 /**
  * Created by GXD on 2018/1/12.
  * 一个最简单的express框架
+ * 运行：
+ * node firstExpress.js
+ * 在浏览器地址栏输入：http://0.0.0.0:8082
+ *
+ * express最重要的两个参数就是request和response
+ * request对象表示HTTP请求， 包含了请求查询字符串， 参数， 内容， HTTP请求头等属性
+ * response对象表示HTTP响应， 即接收到请求时向客户端发送的HTTP响应数据。
  */
 var express = require('express')
 var app = express()
