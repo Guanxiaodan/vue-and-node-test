eca5362ff19e2c5b0e6bf7f9a403673d2ccceaa8
关晓丹 <GXD9983@163.com>
2018-01-24 19:13:41 +0800

    xxx

--- src/tryNode/express/post.js
+++ src/tryNode/express/post.js
@@ -1,4 +1,9 @@
 /**
  * Created by GXD on 2018/1/24.
- *
+ * POST请求
  */
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+
+// 创建application/x-www-form-urlencoded 编码解析
