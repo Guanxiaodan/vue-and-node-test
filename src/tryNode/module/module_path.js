bc7093f721896ce076b7dca53863dbcbe7f9cef9
关晓丹 <GXD9983@163.com>
2018-05-30 18:52:21 +0800

    文件定位

---
+++ src/tryNode/module/module_path.js
@@ -0,0 +1,22 @@
+/**
+ * Created by GXD on 2018/5/30.
+ * node模块实现-模块路径
+ */
+'use strict';
+console.log('module.paths:', module.paths);
+
+/*
+* 运行结果：
+* module.paths: [ '/Users/guanxiaodan/WebstormProjects/gxd/myTest/vue-and-node-test/src/tryNode/module/node_modules',
+ '/Users/guanxiaodan/WebstormProjects/gxd/myTest/vue-and-node-test/src/tryNode/node_modules',
+ '/Users/guanxiaodan/WebstormProjects/gxd/myTest/vue-and-node-test/src/node_modules',
+ '/Users/guanxiaodan/WebstormProjects/gxd/myTest/vue-and-node-test/node_modules',
+ '/Users/guanxiaodan/WebstormProjects/gxd/myTest/node_modules',
+ '/Users/guanxiaodan/WebstormProjects/gxd/node_modules',
+ '/Users/guanxiaodan/WebstormProjects/node_modules',
+ '/Users/guanxiaodan/node_modules',
+ '/Users/node_modules',
+ '/node_modules' ]
+*
+*
+* */
